deb4e0738988b73cba6ef0a0e5dfedf72b58e2b1
Christian Pengu <pengu@goinfoteam.it>
2024-01-15 17:17:35 +0100

    modifica 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Hi! string Length it's greater than 1";
+    return;
   }
 }
